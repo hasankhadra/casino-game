4d4067b116ee17751c226f79685d92f6d6c03d82
hadi <hadi.mohammad.saleh1@gmail.com>
2022-07-19 23:40:39 +0300

    init subgraph

---
+++ casino-evm/subgraph/src/casino.ts
@@ -0,0 +1,62 @@
+import { BigInt } from "@graphprotocol/graph-ts"
+import { Casino, StaticPrizeChange } from "../generated/Casino/Casino"
+import { StaticPrize } from "../generated/schema"
+
+export function handleStaticPrizeChange(event: StaticPrizeChange): void {
+  // Entities can be loaded from the store using a string ID; this ID
+  // needs to be unique across all entities of the same type
+  let entity = StaticPrize.load(event.transaction.from.toHex())
+
+  // Entities only exist after they have been saved to the store;
+  // `null` checks allow to create entities on demand
+  if (!entity) {
+    entity = new StaticPrize(event.transaction.from.toHex())
+
+    // Entity fields can be set using simple assignments
+    entity.newStaticPrize = BigInt.fromI32(0)
+  }
+
+  // Entity fields can be set based on event parameters
+  entity.newStaticPrize = event.params.newStaticPrize
+
+  // Entities can be written to the store with `.save()`
+  entity.save()
+
+  // Note: If a handler doesn't require existing field values, it is faster
+  // _not_ to load the entity from the store. Instead, create it fresh with
+  // `new Entity(...)`, set the fields that should be updated and save the
+  // entity back to the store. Fields that were not set or unset remain
+  // unchanged, allowing for partial updates to be applied.
+
+  // It is also possible to access smart contracts from mappings. For
+  // example, the contract that has emitted the event can be connected to
+  // with:
+  //
+  // let contract = Contract.bind(event.address)
+  //
+  // The following functions can then be called on this contract to access
+  // state variables and other data:
+  //
+  // - contract.biddingAmount(...)
+  // - contract.getMax(...)
+  // - contract.numbersRange(...)
+  // - contract.owner(...)
+  // - contract.ownerIncomePercentage(...)
+  // - contract.pot(...)
+  // - contract.potIncomePercentage(...)
+  // - contract.potPrizePercentage(...)
+  // - contract.queue(...)
+  // - contract.queueAvailableFunds(...)
+  // - contract.queueBack(...)
+  // - contract.queueFront(...)
+  // - contract.queueLength(...)
+  // - contract.queuePrizeAmount(...)
+  // - contract.queueTakenAmount(...)
+  // - contract.requestIdToAddress(...)
+  // - contract.requestIdToAddressTesting(...)
+  // - contract.requestIdToGuess(...)
+  // - contract.requestIdToGuessTesting(...)
+  // - contract.staticPrize(...)
+  // - contract.timeToLive(...)
+  // - contract.toBePaid(...)
+}
