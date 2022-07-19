b0d0add4168362b3bf08f5a1dc1e332386a8dd0d
hasankhadra <59062695+hasankhadra@users.noreply.github.com>
2022-07-19 22:29:25 +0300

    Fixed mumbai alchemy api

--- casino-evm/hardhat.config.ts
+++ casino-evm/hardhat.config.ts
@@ -16,7 +16,7 @@ const config: HardhatUserConfig = {
   },
   networks: {
     mumbai: {
-      url: `https://polygon-mumbai.g.alchemy.com/v2${process.env.ALCHEMY_API_KEY}`,
+      url: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
       accounts: [process.env.PRIVATE_KEY_1, process.env.PRIVATE_KEY_2] 
     },
   }
