96b798c7cc8910d5e19ac1e557a7233333bb3d16
alimansour0002 <alimansour0002@gmail.com>
2022-07-22 15:39:00 +0300

    connect wallet & read from contract

--- frontend/src/hooks/queueAvailableFunds.tsx
+++ frontend/src/hooks/queueAvailableFunds.tsx
@@ -2,13 +2,14 @@ import {ethers} from 'ethers'
 import React, { useState, useEffect } from 'react';
 import { useContractRead } from 'wagmi'
 import {abi} from '../utils/config'
-export function useGetqueuePrizeAmount() {
+export function queueAvailableFunds() {
     const { data, isError, isLoading ,status} = useContractRead({
-        addressOrName: '0x0fcB81c2CD4c17072B5eDb4e899eaD4c22F07c63',
+        addressOrName: '0xb4926B66ee76214773F96FfE58840b0e1c085dD6',
         contractInterface: abi,
-        functionName: '',
+        functionName: 'queueAvailableFunds',
         chainId: 80001
       })
+      console.log(data);
       if(!data)return 2;
       return data.toString();
   }
