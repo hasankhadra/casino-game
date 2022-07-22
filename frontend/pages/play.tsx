2a9b44cb862c1314ead07f6fb1957a6045d42997
alimansour0002 <alimansour0002@gmail.com>
2022-07-22 23:07:23 +0300

    added read from contract hooks

--- frontend/pages/play.tsx
+++ frontend/pages/play.tsx
@@ -3,22 +3,41 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Link from "next/link";
 import Header from "../src/components/header/header"
-import {queueAvailableFunds} from '../src/hooks/queueAvailableFunds'
-import {pot} from '../src/hooks/pot'
+import { queueAvailableFunds } from '../src/hooks/queueAvailableFunds'
+import { pot } from '../src/hooks/pot'
+import { staticPrize } from '../src/hooks/staticPrize'
+import { biddingAmount } from '../src/hooks/biddingAmount'
+import { numbersRange } from '../src/hooks/numbersRange'
+import { timeToLive } from '../src/hooks/timeToLive'
 const Home: NextPage = () => {
   return (
     <div>
-      <Header/>
-    <h1>Play</h1>
-    <div>{
-    "queueAvailableFunds : "}
-      {queueAvailableFunds()}
-    </div>
-    <div>{
-    "pot : "}
-      {pot()}
-    </div>
-
+      <Header />
+      <h1>Play</h1>
+      <div>{
+        "queueAvailableFunds : "}
+        {queueAvailableFunds()}
+      </div>
+      <div>{
+        "pot : "}
+        {pot()}
+      </div>
+      <div>{
+        "staticPrize : "}
+        {staticPrize()}
+      </div>
+      <div>{
+        "biddingAmount : "}
+        {biddingAmount()}
+      </div>
+      <div>{
+        "numbersRange : "}
+        {numbersRange()}
+      </div>
+      <div>{
+        "timeToLive : "}
+        {timeToLive()}
+      </div>
     </div>
   )
 }
